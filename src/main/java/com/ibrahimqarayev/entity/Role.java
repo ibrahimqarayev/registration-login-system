cd901515317cbac2a5d79ee98c2643bab3cfc5e3
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-18 16:10:19 +0400

    Role

---
+++ src/main/java/com/ibrahimqarayev/entity/Role.java
@@ -0,0 +1,32 @@
+package com.ibrahimqarayev.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users = new ArrayList<>();
+
+
+}
