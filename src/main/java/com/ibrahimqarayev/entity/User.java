974e7722f67e6a7bb477ba50b09776b46aa804f1
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-18 16:10:05 +0400

    User

--- src/main/java/com/ibrahimqarayev/entity/User.java
+++ src/main/java/com/ibrahimqarayev/entity/User.java
@@ -1,4 +1,50 @@
 package com.ibrahimqarayev.entity;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "firstname", nullable = false)
+    private String name;
+
+    @Column(name = "lastname", nullable = false)
+    private String surname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = { @JoinColumn(name = "user_id", referencedColumnName = "id") },
+            inverseJoinColumns = { @JoinColumn(name = "role_id", referencedColumnName = "id") })
+
+    private List<Role> roles=new ArrayList<>();
+
 }
