7ba0f28a4ff74565382476e29224cf8f046ea6d1
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-18 16:11:10 +0400

    RoleRepository

---
+++ src/main/java/com/ibrahimqarayev/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.ibrahimqarayev.repository;
+
+import com.ibrahimqarayev.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
