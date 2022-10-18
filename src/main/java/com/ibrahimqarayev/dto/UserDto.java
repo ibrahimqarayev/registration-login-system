82372b517d2e3041c659ba743d259750bbbf8379
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-18 16:23:08 +0400

    UserDto

---
+++ src/main/java/com/ibrahimqarayev/dto/UserDto.java
@@ -0,0 +1,31 @@
+package com.ibrahimqarayev.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserDto {
+
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty(message = "Email bosh ola bilmez")
+    @Email
+    private String email;
+    @NotEmpty(message = "Shifre bosh ola bilmez")
+    private String password;
+
+
+
+}
