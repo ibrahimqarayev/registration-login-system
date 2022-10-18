cdf81d52d8342466ab3ee8f7a925b4c2b08fad01
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-18 16:10:42 +0400

    Main class

--- src/main/java/com/ibrahimqarayev/RegistrationLoginSystemApplication.java
+++ src/main/java/com/ibrahimqarayev/RegistrationLoginSystemApplication.java
@@ -2,6 +2,7 @@ package com.ibrahimqarayev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 
 @SpringBootApplication
 public class RegistrationLoginSystemApplication {
