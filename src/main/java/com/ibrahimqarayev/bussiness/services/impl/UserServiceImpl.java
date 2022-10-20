f40ba0070fd4e35b694729b7db594c0369d80b80
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-20 14:42:28 +0400

    User Service Implementation

---
+++ src/main/java/com/ibrahimqarayev/bussiness/services/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ibrahimqarayev.bussiness.services.impl;
+
+import com.ibrahimqarayev.bussiness.dto.UserDto;
+import com.ibrahimqarayev.data.entity.User;
+import com.ibrahimqarayev.data.repository.repository.RoleRepository;
+import com.ibrahimqarayev.data.repository.repository.UserRepository;
+import com.ibrahimqarayev.bussiness.services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void saveUser(UserDto userDto) {
+
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        User user = userRepository.findByEmail(email);
+        return user;
+    }
+
+    @Override
+    public List<UserDto> findAllUsers() {
+        return null;
+    }
+}
