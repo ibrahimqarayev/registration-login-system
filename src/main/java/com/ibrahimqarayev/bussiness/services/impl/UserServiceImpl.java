02bf941e37fc2c84cbfcccf3dc45b5a2e2bc0d83
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-21 12:00:25 +0400

    UserServiceImpl

--- src/main/java/com/ibrahimqarayev/bussiness/services/impl/UserServiceImpl.java
+++ src/main/java/com/ibrahimqarayev/bussiness/services/impl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.ibrahimqarayev.bussiness.services.impl;
 
 import com.ibrahimqarayev.bussiness.dto.UserDto;
+import com.ibrahimqarayev.data.entity.Role;
 import com.ibrahimqarayev.data.entity.User;
-import com.ibrahimqarayev.data.repository.repository.RoleRepository;
-import com.ibrahimqarayev.data.repository.repository.UserRepository;
+import com.ibrahimqarayev.data.repository.RoleRepository;
+import com.ibrahimqarayev.data.repository.UserRepository;
 import com.ibrahimqarayev.bussiness.services.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,7 +20,9 @@ public class UserServiceImpl implements UserService {
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
@@ -25,18 +30,46 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(UserDto userDto) {
+        User user = new User();
+        user.setName(userDto.getFirstName() + " " + userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        // encrypt the password using spring security
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
+        Role role = roleRepository.findByName("ROLE_ADMIN");
+        if (role == null) {
+            role = checkRoleExist();
+        }
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
     }
 
     @Override
     public User findUserByEmail(String email) {
-        User user = userRepository.findByEmail(email);
-        return user;
+        return userRepository.findByEmail(email);
     }
-
 
     @Override
     public List<UserDto> findAllUsers() {
-        return null;
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map((user) -> mapToUserDto(user))
+                .collect(Collectors.toList());
     }
+
+    private UserDto mapToUserDto(User user) {
+        UserDto userDto = new UserDto();
+        String[] str = user.getName().split(" ");
+        userDto.setFirstName(str[0]);
+        userDto.setLastName(str[1]);
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+
+    private Role checkRoleExist() {
+        Role role = new Role();
+        role.setName("ROLE_ADMIN");
+        return roleRepository.save(role);
+    }
+
 }
