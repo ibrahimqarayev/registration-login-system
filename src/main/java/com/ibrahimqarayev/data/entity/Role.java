0910a572fa05ebe4c2ff05da12b402fdb4f641f8
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-10-21 11:58:05 +0400

    Role

--- src/main/java/com/ibrahimqarayev/data/entity/Role.java
+++ src/main/java/com/ibrahimqarayev/data/entity/Role.java
@@ -9,22 +9,18 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Getter
 @Setter
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user_roles")
+@Entity
+@Table(name = "roles")
 public class Role {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "name", nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String name;
-
     @ManyToMany(mappedBy = "roles")
     private List<User> users = new ArrayList<>();
 
